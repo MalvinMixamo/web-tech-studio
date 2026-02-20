1700077918ed61dc09a9e815f2630410ca814810
Kipin.Kipon <malvinmixamo@gmail.com>
2026-02-20 21:16:17 +0700

    done api course, chapter GET & POST only

---
+++ app/api/courses/[courseId]/chapter/route.js
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+    const { courseId } = await params
+    try{
+        const [rows] = await db.query(
+            `SELECT course.title AS nama_kursus, chapter.* FROM course 
+             LEFT JOIN chapter ON course.id = chapter.course_id 
+             WHERE course.id = ?`, 
+            [courseId]
+        )
+        return NextResponse.json(rows, {status: 200})
+    }catch(err){
+        return NextResponse.json(err, {status: 500})
+    }
+}
+export async function POST(request, { params }) {
+    const { courseId } = await params
+    try{
+        const { title, ordinal } = await request.json()
+        await db.query(`INSERT INTO chapter (course_id, title, ordinal) VALUES (?, ?, ?)`, [courseId, title, ordinal])
+        return NextResponse.json({message: 'data berhasil dibuat!'}, {status: 200})
+    }catch(err){
+        return NextResponse.json(err, {status: 500})
+    }
+}
